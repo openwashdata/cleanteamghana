e47fdf46c21625bfbc52601c23b8c8106f4d5524
Skorik  Sophia <sskorik@ethz.ch>
2023-10-25 09:56:09 +0200

    data processing complete

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,20 @@
-## code to prepare `DATASET` dataset goes here
+# description -------------------------------------------------------------
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# R script to process uploaded raw data into a tidy dataframe
+
+# load packages -----------------------------------------------------------
+
+library(readr)
+
+# read data ---------------------------------------------------------------
+
+dataset <- read_csv("data-raw/CTG_data_anonymized.csv")
+
+# tidy data ---------------------------------------------------------------
+
+## code to prepare a tidy, analysis-ready dataset goes here
+
+# write data --------------------------------------------------------------
+
+usethis::use_data(dataset, overwrite = TRUE)
+
