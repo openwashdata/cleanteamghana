7b7543a17925b15b4ff62ba1af739df5bd79a65b
Skorik  Sophia <sskorik@ethz.ch>
2023-10-25 09:57:26 +0200

    add dictionary

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -5,6 +5,7 @@
 # load packages -----------------------------------------------------------
 
 library(readr)
+library(tidyverse)
 
 # read data ---------------------------------------------------------------
 
@@ -12,9 +13,18 @@ dataset <- read_csv("data-raw/CTG_data_anonymized.csv")
 
 # tidy data ---------------------------------------------------------------
 
+cleanteamghana <- dataset |>
+  select(
+    age = age_of_respondent,
+    area = area,
+    toilet = able_to_observe_toilet,
+    money_toilet = cant_afford_toilet
+  )
 ## code to prepare a tidy, analysis-ready dataset goes here
 
 # write data --------------------------------------------------------------
 
-usethis::use_data(dataset, overwrite = TRUE)
-
+usethis::use_data(cleanteamghana, overwrite = TRUE)
+fs::dir_create(here::here("inst", "extdata")) #exports dataset in R
+write_csv(cleanteamghana, here::here("inst/extdata/cleanteamghana.csv")) #exports dataset in csv and excel
+openxlsx::write.xlsx(cleanteamghana,"inst/extdata/cleanteamghana.xlsx")
