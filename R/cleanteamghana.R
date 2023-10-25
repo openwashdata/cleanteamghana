0c44b309013308f13180a1e8c3c77d43b2fa971e
Skorik  Sophia <sskorik@ethz.ch>
2023-10-25 10:15:35 +0200

    add description of data

---
+++ R/cleanteamghana.R
@@ -0,0 +1,15 @@
+#ADD TITLE
+#' Ghana Clean Team Survey
+#'
+#'ADD DESCRIPTION
+#' TBD
+#'
+#' @format A tibble with 584 rows and 4 variables:
+
+#' \describe{
+#'   \item{age}{ Age of repondent}
+#'   \item{area}{ Origin area in Ghana from the respondent}
+#'   \item{toilet}{ Question: Are you able to observe a toilet?}
+#'   \item{money_toilet}{ Are you able to afford a toilet?}
+#' }
+"cleanteamghana"
